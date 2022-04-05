dcac2257ca252fe033b6208277f094a410c77fe0
João Pedro Marinho <marinhojp@outloo.com>
2022-04-05 19:48:52 -0300

    Add Initial SpringBoot Web Project files

---
+++ professor-allocation-joaopedro/src/main/java/com/project/professorallocation/ProfessorAllocationJoaopedroApplication.java
@@ -0,0 +1,13 @@
+package com.project.professorallocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationJoaopedroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationJoaopedroApplication.class, args);
+	}
+
+}
