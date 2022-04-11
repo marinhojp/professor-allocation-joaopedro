ac04f9c5c9b14eecce4340b3f4950837c9903668
João Pedro Marinho <marinhojp@outloo.com>
2022-04-11 19:51:14 -0300

    Add Entities Course and Allocation

---
+++ professor-allocation-joaopedro/src/main/java/com/project/professorallocation/model/Allocation.java
@@ -0,0 +1,98 @@
+package com.project.professorallocation.model;
+
+import java.time.DayOfWeek;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "allocation")
+public class Allocation {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, name = "day")
+	private DayOfWeek dayOfWeek;
+	
+	@Temporal(TemporalType.TIME)
+	@Column(nullable = false)
+	private Date startHour;
+
+	@Temporal(TemporalType.TIME)
+	@Column(nullable = false)
+	private Date endHour;
+
+	private Long professorId;
+
+	private Long courseId;
+
+	public Allocation() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public DayOfWeek getDayOfWeek() {
+		return dayOfWeek;
+	}
+
+	public void setDayOfWeek(DayOfWeek dayOfWeek) {
+		this.dayOfWeek = dayOfWeek;
+	}
+
+	public Date getStartHour() {
+		return startHour;
+	}
+
+	public void setStartHour(Date startHour) {
+		this.startHour = startHour;
+	}
+
+	public Date getEndHour() {
+		return endHour;
+	}
+
+	public void setEndHour(Date endHour) {
+		this.endHour = endHour;
+	}
+
+	public Long getProfessorId() {
+		return professorId;
+	}
+
+	public void setProfessorId(Long professorId) {
+		this.professorId = professorId;
+	}
+
+	public Long getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(Long courseId) {
+		this.courseId = courseId;
+	}
+
+	@Override
+	public String toString() {
+		return "Allocation [id=" + id + ", dayOfWeek=" + dayOfWeek + ", startHour=" + startHour + ", endHour=" + endHour
+				+ ", professorId=" + professorId + ", courseId=" + courseId + "]";
+	}
+
+}
---
+++ professor-allocation-joaopedro/src/main/java/com/project/professorallocation/model/Course.java
@@ -0,0 +1,45 @@
+package com.project.professorallocation.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "course")
+public class Course {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "name", length = 100, unique = true, nullable = false)
+	private String name;
+
+	public Course() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", name=" + name + "]";
+	}
+
+}
