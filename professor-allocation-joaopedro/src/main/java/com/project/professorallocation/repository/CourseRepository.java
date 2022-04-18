af2d965850f1c0873be0d0f5bf82b5819be7dbc5
João Pedro Marinho <marinhojp@outloo.com>
2022-04-18 18:59:07 -0300

    Edited CourseRepository

--- professor-allocation-joaopedro/src/main/java/com/project/professorallocation/repository/CourseRepository.java
+++ professor-allocation-joaopedro/src/main/java/com/project/professorallocation/repository/CourseRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.project.professorallocation.model.Course;
 
 @Repository
-public interface CourseRpository extends JpaRepository<Course, Long> {
+public interface CourseRepository extends JpaRepository<Course, Long> {
 
 }
