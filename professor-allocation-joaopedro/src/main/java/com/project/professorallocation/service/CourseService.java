f8d6b7e2752eb7ecdc829c2592b1f98dae203b17
João Pedro Marinho <marinhojp@outloo.com>
2022-04-18 21:03:14 -0300

    Edited CourseService code

--- professor-allocation-joaopedro/src/main/java/com/project/professorallocation/service/CourseService.java
+++ professor-allocation-joaopedro/src/main/java/com/project/professorallocation/service/CourseService.java
@@ -1,7 +1,10 @@
 package com.project.professorallocation.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.project.professorallocation.model.Course;
 import com.project.professorallocation.repository.CourseRepository;
 
 @Service
@@ -12,6 +15,39 @@ public class CourseService {
 		super();
 		this.repository = repository;
 	}
-	
-	
+
+	public Course create(Course course) {
+		course.setId(null);
+		return saveInternal(course);
+	}
+
+	public Course update(Course course) {
+		Long id = course.getId();
+		if (id == null || !repository.existsById(id)) {
+			return null;
+		} else {
+			return saveInternal(course);
+		}
+	}
+
+	private Course saveInternal(Course course) {
+		Course insertedCourse = repository.save(course);
+		return insertedCourse;
+	}
+
+	public void deleteById(Long id) {
+		if (repository.existsById(id)) {
+			repository.deleteById(id);
+		}
+
+	}
+
+	public Course findById(Long id) {
+		return repository.findById(id).orElse(null);
+
+	}
+
+	public List<Course> findAll() {
+		return repository.findAll();
+	}
 }
