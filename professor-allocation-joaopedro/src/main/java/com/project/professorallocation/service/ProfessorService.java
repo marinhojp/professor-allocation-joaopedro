6c74f46478919e90cc365ed2105987bf75a27c0b
João Pedro Marinho <marinhojp@outloo.com>
2022-04-21 21:51:10 -0300

    Add code to ProfessorService

--- professor-allocation-joaopedro/src/main/java/com/project/professorallocation/service/ProfessorService.java
+++ professor-allocation-joaopedro/src/main/java/com/project/professorallocation/service/ProfessorService.java
@@ -1,7 +1,10 @@
 package com.project.professorallocation.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.project.professorallocation.model.Professor;
 import com.project.professorallocation.repository.ProfessorRepository;
 
 @Service
@@ -12,7 +15,42 @@ public class ProfessorService {
 		super();
 		this.repository = repository;
 	}
-	
-	
+
+	public Professor create(Professor professor) {
+		professor.setId(null);
+		return saveInternal(professor);
+
+	}
+
+	public Professor update(Professor professor) {
+		Long id = professor.getId();
+		if (id == null || !repository.existsById(id)) {
+
+			return null;
+		} else {
+			return saveInternal(professor);
+		}
+	}
+
+	private Professor saveInternal(Professor professor) {
+		Professor insertedProfessor = repository.save(professor);
+		return insertedProfessor;
+	}
+
+	public void deleteById(Long id) {
+		if (repository.existsById(id)) {
+			repository.deleteById(id);
+		}
+
+	}
+
+	public Professor findById(Long id) {
+		return repository.findById(id).orElse(null);
+
+	}
+
+	public List<Professor> findAll() {
+		return repository.findAll();
+	}
 
 }
