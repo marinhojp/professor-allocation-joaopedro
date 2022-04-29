ce86ea0d77f4a5391d693710fffd169f891497e2
João Pedro Marinho <marinhojp@outloo.com>
2022-04-28 21:03:26 -0300

    Add update and delete methods to DepartmentController

--- professor-allocation-joaopedro/src/main/java/com/project/professorallocation/controller/DepartmentController.java
+++ professor-allocation-joaopedro/src/main/java/com/project/professorallocation/controller/DepartmentController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -49,13 +51,36 @@ public class DepartmentController {
 		}
 
 	}
-	
+
 	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseStatus(HttpStatus.CREATED)
-	public ResponseEntity<Department> create(@RequestBody Department dept){
+	public ResponseEntity<Department> create(@RequestBody Department dept) {
 		Department item = service.create(dept);
-		
+
 		return new ResponseEntity<>(item, HttpStatus.CREATED);
+	}
+
+	@PutMapping(path = "/{dept_id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<Department> update(@PathVariable(name = "dept_id") Long id, @RequestBody Department dept) {
+		dept.setId(id);
+		Department item = service.update(dept);
+
+		if (item == null) {
+
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<>(item, HttpStatus.OK);
+		}
+	}
+
+	@DeleteMapping(path = "/{dept_id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public ResponseEntity<Void> delete(@PathVariable(name = "dept_id") Long id) {
+		service.deleteById(id);
+
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
 	}
 
 }
