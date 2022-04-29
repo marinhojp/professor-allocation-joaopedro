b386796e6184b4696ebf14aae7d94e848b4c7d8d
João Pedro Marinho <marinhojp@outloo.com>
2022-04-28 21:02:50 -0300

    Add Controller Layer for Allocation

---
+++ professor-allocation-joaopedro/src/main/java/com/project/professorallocation/controller/AllocationController.java
@@ -0,0 +1,34 @@
+package com.project.professorallocation.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.professorallocation.model.Allocation;
+import com.project.professorallocation.service.AllocationService;
+
+@RestController
+@RequestMapping(path = "/allocations")
+public class AllocationController {
+	
+	private final AllocationService service;
+
+	public AllocationController(AllocationService service) {
+		super();
+		this.service = service;
+	}
+	
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<List<Allocation>> findAll(){
+		List<Allocation> allAllocations = service.findAll();
+		
+		return new ResponseEntity<>(allAllocations, HttpStatus.OK);
+	}
+}
