2a2ce8204a38f31b35f0978f44bcf693a7555ea1
João Pedro Marinho <marinhojp@outloo.com>
2022-05-02 17:56:44 -0300

    Add update and delete methods to CourseController and ProfessorController

--- professor-allocation-joaopedro/src/main/java/com/project/professorallocation/controller/CourseController.java
+++ professor-allocation-joaopedro/src/main/java/com/project/professorallocation/controller/CourseController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -56,6 +58,29 @@ public class CourseController {
 		Course item = service.create(course);
 
 		return new ResponseEntity<>(item, HttpStatus.CREATED);
+
+	}
+
+	@PutMapping(path = "/{course_id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<Course> update(@PathVariable(name = "course_id") Long id, @RequestBody Course course) {
+		course.setId(id);
+		Course item = service.update(course);
+
+		if (item == null) {
+
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<>(item, HttpStatus.OK);
+		}
+	}
+
+	@DeleteMapping(path = "/{course_id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public ResponseEntity<Void> delete(@PathVariable(name = "course_id") Long id) {
+		service.deleteById(id);
+
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 
 	}
 
--- professor-allocation-joaopedro/src/main/java/com/project/professorallocation/controller/ProfessorController.java
+++ professor-allocation-joaopedro/src/main/java/com/project/professorallocation/controller/ProfessorController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -56,6 +58,30 @@ public class ProfessorController {
 		Professor item = service.create(prof);
 
 		return new ResponseEntity<>(item, HttpStatus.CREATED);
+
+	}
+
+	@PutMapping(path = "/{prof_id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<Professor> update(@PathVariable(name = "prof_id") Long id, @RequestBody Professor prof) {
+		prof.setId(id);
+		Professor item = service.update(prof);
+
+		if (item == null) {
+
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<>(item, HttpStatus.OK);
+		}
+	}
+
+	@DeleteMapping(path = "/{prof_id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public ResponseEntity<Void> delete(@PathVariable(name = "prof_id") Long id) {
+
+		service.deleteById(id);
+
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 
 	}
 
